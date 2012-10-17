563da8c19001847b977c272965482abd8432cff5
Oleksandr Lukiantsev <alexander.lukyantsev@gmail.com>
2012-10-16 23:55:36 -0700

    added #1.7

---
+++ CrackingCodingInterview.Test/Chapter1/SetRowAndColumnToZeroWhereCellIsZeroTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using CrackingCodingInterview.Chapter1;
+using NUnit.Framework;
+
+namespace CrackingCodingInterview.Test.Chapter1
+{
+    [TestFixture]
+    public class SetRowAndColumnToZeroWhereCellIsZeroTest : TestBase
+    {
+        [Test]
+        [TestCaseSource("TestCases")]
+        public int[,] Test(int[,] matrix)
+        {
+            var cut = new SetRowAndColumnToZeroWhereCellIsZero();
+            return RunTest(matrix, cut);
+        }
+
+        public static IEnumerable TestCases
+        {
+            get
+            {
+                yield return new TestCaseData(null).Throws(typeof (ArgumentNullException)).SetName("Null");
+                yield return new TestCaseData(new int[0,0]).Returns(new int[0,0]).SetName("Empty");
+
+                var matrix = new int[2,2];
+                matrix[0, 0] = 0;
+                matrix[0, 1] = 1;
+                matrix[1, 0] = 2;
+                matrix[1, 1] = 2;
+
+                var returns = new int[2,2];
+                matrix[0, 0] = 0;
+                matrix[0, 1] = 0;
+                matrix[1, 0] = 0;
+                matrix[1, 1] = 2;
+
+                yield return new TestCaseData(matrix).Returns(returns).SetName("One zero");
+            }
+        }
+    }
+
+
+}
---
+++ CrackingCodingInterview/Chapter1/SetRowAndColumnToZeroWhereCellIsZero.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace CrackingCodingInterview.Chapter1
+{
+    [Reference(
+        Page = 48
+        , Number = "1.7"
+        , Description = "Write an algorithm such that " +
+                        "if an element in an MxN matrix is 0" +
+                        ", its entire row and column is set to 0"
+        )]
+    public class SetRowAndColumnToZeroWhereCellIsZero : Algorithm<int[,], int[,]>
+    {
+        protected override int OnInitComplexity(int[,] arg)
+        {
+            if (arg == null) throw new ArgumentNullException("arg");
+            return arg.Length;
+        }
+
+        protected override int[,] OnExecute(int[,] arg)
+        {
+            if (arg == null)
+                throw new ArgumentNullException("arg");
+
+            var rowsCount = arg.GetLength(0);
+            var columnsCount = arg.GetLength(1);
+
+            var zeroRows = new bool[rowsCount];
+            var zeroColumns = new bool[columnsCount];
+
+            for (var i = 0; i < rowsCount; i++)
+            {
+                IncrementIteration();
+                for (var j = 0; j < columnsCount; j++)
+                {
+                    IncrementIteration();
+                    if (arg[i, j] != 0) continue;
+                    zeroRows[i] = true;
+                    zeroColumns[j] = true;
+                }
+            }
+
+            for (var i = 0; i < rowsCount; i++)
+            {
+                IncrementIteration();
+                for (var j = 0; j < columnsCount; j++)
+                {
+                    IncrementIteration();
+                    if (zeroRows[i] || zeroColumns[j])
+                    {
+                        arg[i, j] = 0;
+                    }
+                }
+            }
+
+            return arg;
+        }
+    }
+}
