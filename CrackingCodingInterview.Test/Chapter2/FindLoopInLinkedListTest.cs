5a78e2c0442691ac18042909481eddb02aab4b3b
Oleksandr Lukiantsev <alexander.lukyantsev@gmail.com>
2012-11-01 00:21:25 -0700

    added #2.5

---
+++ CrackingCodingInterview.Test/Chapter2/FindLoopInLinkedListTest.cs
@@ -0,0 +1,31 @@
+﻿using CrackingCodingInterview.Chapter2;
+using NUnit.Framework;
+
+namespace CrackingCodingInterview.Test.Chapter2
+{
+
+    [TestFixture]
+    public class FindLoopInLinkedListTest : TestBase
+    {
+        [Test]
+        public void Test()
+        {
+            var a = new LinkedListNode<int> {Data = 1};
+            var b = new LinkedListNode<int> {Data = 2};
+            var c = new LinkedListNode<int> {Data = 3};
+            var d = new LinkedListNode<int> {Data = 4};
+            var e = new LinkedListNode<int> {Data = 5};
+            var f = new LinkedListNode<int> {Data = 6};
+            var g = new LinkedListNode<int> {Data = 7};
+            a.Next = b;
+            b.Next = c;
+            c.Next = d;
+            d.Next = e;
+            e.Next = f;
+            f.Next = g;
+            g.Next = c;
+            var linkedListNode = RunTest(a, new FindLoopInLinkedList());
+            Assert.AreEqual(c, linkedListNode);
+        }
+    }
+}
---
+++ CrackingCodingInterview/Chapter2/FindLoopInLinkedList.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace CrackingCodingInterview.Chapter2
+{
+
+    [Reference(
+        Page = 50
+        , Number = "2.5"
+        , Description = "Given a circular linked list, implement an algorithm" +
+                        "which returns node at the beginning of the loop")]
+    public class FindLoopInLinkedList : Algorithm<LinkedListNode<int>, LinkedListNode<int>>
+    {
+        protected override int OnInitComplexity(LinkedListNode<int> arg)
+        { 
+            if (arg == null) throw new ArgumentNullException("arg");
+            return -1;
+        }
+
+        protected override LinkedListNode<int> OnExecute(LinkedListNode<int> arg)
+        {
+            if (arg == null) throw new ArgumentNullException("arg");
+            LinkedListNode<int> slow = arg;
+            LinkedListNode<int> fast = arg;
+
+            while (fast.Next != null)
+            {
+                slow = slow.Next;
+                fast = fast.Next.Next;
+
+                if (Equals(slow, fast))
+                    break;
+            }
+            
+            if (fast.Next == null)
+            {
+                return null;
+            }
+
+            fast = arg;
+
+            while(!Equals(fast, slow))
+            {
+                fast = fast.Next;
+                slow = slow.Next;
+            }
+
+            return fast;
+        }
+    }
+}
