7c70f15b2b383e3896a35c6623f31cc09fea110e
Oleksandr Lukiantsev <alexander.lukyantsev@gmail.com>
2012-11-08 00:06:26 -0800

    added #3.4

---
+++ CrackingCodingInterview.Test/Chapter3/HanoiTowerTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+using CrackingCodingInterview.Chapter3;
+using NUnit.Framework;
+
+namespace CrackingCodingInterview.Test.Chapter3
+{
+    [TestFixture]
+    public class HanoiTowerTest
+    {
+        [Test]
+        public void Test()
+        {
+            var towers = new HanoiTower[3];
+            for (int i = 0; i < towers.Length; i++)
+            {
+                towers[i] = new HanoiTower(i.ToString(CultureInfo.InvariantCulture));
+            }
+
+            const int n = 5;
+
+            for (int i = n - 1; i >= 0; i--)
+            {
+                towers[0].Add(i);
+            }
+
+            towers[0].MoveDisks(n, towers[2], towers[1]);
+
+            CollectionAssert.AreEqual(new int[]{}, towers[0].ToArray());
+            CollectionAssert.AreEqual(new int[]{}, towers[1].ToArray());
+            CollectionAssert.AreEqual(new[]{0, 1, 2, 3, 4}, towers[2].ToArray());
+            
+        }
+    }
+}
---
+++ CrackingCodingInterview/Chapter3/HanoiTower.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace CrackingCodingInterview.Chapter3
+{
+    [Reference(
+        Page = 52
+        , Number = "3.4"
+        , Description = "Classic Twoers of Hanoi. " +
+                        "Write program to move the disks from the first rod to the last using Stacks")]
+    public class HanoiTower
+    {
+
+        private readonly Stack<int> _disks = new Stack<int>();
+
+        public HanoiTower(string name)
+        {
+            if (name == null) throw new ArgumentNullException("name");
+            Name = name;
+        }
+
+        public string Name { get; private set; }
+
+        public void MoveDisks(int disk, HanoiTower destination, HanoiTower buffer)
+        {
+            if (disk <= 0) return;
+            MoveDisks(disk - 1, buffer, destination);
+            MoveTopTo(destination);
+            buffer.MoveDisks(disk - 1, destination, this);
+        }
+
+        public int[] ToArray()
+        {
+            return _disks.ToArray();
+        }
+
+        private void MoveTopTo(HanoiTower destination)
+        {
+            int pop = _disks.Pop();
+            destination.Add(pop);
+            Trace.TraceInformation("Move disk {0} from {1} to {2}", pop, Name, destination.Name);
+        }
+
+        public void Add(int pop)
+        {
+            if ((_disks.Count > 0) && (_disks.Peek() <= pop))
+                throw new ArgumentException("pop", "pop");
+
+            _disks.Push(pop);
+        }
+    }
+}
