c293a2fe89177a893a7b3c160411f2a3e3ef6996
Oleksandr Lukiantsev <alexander.lukyantsev@gmail.com>
2012-11-10 15:03:37 -0800

    added #3.5

---
+++ CrackingCodingInterview.Test/Chapter3/QueueBasedOnTwoStacksTest.cs
@@ -0,0 +1,39 @@
+﻿using CrackingCodingInterview.Chapter3;
+using NUnit.Framework;
+
+namespace CrackingCodingInterview.Test.Chapter3
+{
+    [TestFixture]
+    public class QueueBasedOnTwoStacksTest
+    {
+        [Test]
+        public void One_Element()
+        {
+            var queue = new QueueBasedOnTwoStacks<int>();
+            queue.Enqueue(1);
+            Assert.AreEqual(1, queue.Dequeue());
+        }
+
+        [Test]
+        public void Two_Elements()
+        {
+            var queue = new QueueBasedOnTwoStacks<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(2);        
+            Assert.AreEqual(1, queue.Dequeue());
+            Assert.AreEqual(2, queue.Dequeue());            
+        }
+
+        [Test]
+        public void Three_Elements()
+        {
+            var queue = new QueueBasedOnTwoStacks<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            Assert.AreEqual(1, queue.Dequeue());
+            Assert.AreEqual(2, queue.Dequeue());
+            Assert.AreEqual(3, queue.Dequeue());
+        }
+    }
+}
---
+++ CrackingCodingInterview/Chapter3/QueueBasedOnTwoStacks.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+
+namespace CrackingCodingInterview.Chapter3
+{
+    [Reference(
+        Page = 52
+        , Number = "3.5"
+        , Description = "Implement a queue which implemens a queue using two stacks")]
+    public class QueueBasedOnTwoStacks<T>
+    {
+        public QueueBasedOnTwoStacks()
+        {
+            _popPeekStack = new Stack<T>();
+            _pushStack = new Stack<T>();
+        }
+
+        public QueueBasedOnTwoStacks(int count)
+        {
+            _popPeekStack = new Stack<T>(count);
+            _pushStack = new Stack<T>(count);
+        }
+
+        public void Enqueue(T item)
+        {
+            _pushStack.Push(item);
+        }
+
+        public T Dequeue()
+        {
+            if (_popPeekStack.Count == 0)
+                while (_pushStack.Count > 0)
+                    _popPeekStack.Push(_pushStack.Pop());
+
+            return _popPeekStack.Pop();
+        }
+
+        private readonly Stack<T> _popPeekStack;
+        private readonly Stack<T> _pushStack;
+    }
+}
