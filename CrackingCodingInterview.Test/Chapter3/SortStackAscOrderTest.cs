8140401b5f6844ce73f089559b90c26743783c98
Oleksandr Lukiantsev <alexander.lukyantsev@gmail.com>
2012-11-13 00:14:43 -0800

    added #3.6

---
+++ CrackingCodingInterview.Test/Chapter3/SortStackAscOrderTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using CrackingCodingInterview.Chapter3;
+using NUnit.Framework;
+
+namespace CrackingCodingInterview.Test.Chapter3
+{
+    [TestFixture]
+    public class SortStackAscOrderTest : TestBase
+    {
+        [Test]
+        [TestCaseSource("TestCases")]
+        public Stack<int> Test(Stack<int> unsorted)
+        {
+            return RunTest(unsorted, new SortStackAscOrder());
+        }
+
+        public static IEnumerable TestCases
+        {
+            get 
+            {
+                yield return new TestCaseData(new Stack<int>(new[] { 2 })).Returns(new Stack<int>(new[] { 2 })).SetName("Some cases");
+                yield return new TestCaseData(new Stack<int>(new[] { 1, 2, 3 })).Returns(new Stack<int>(new[] { 1, 2, 3 })).SetName("Some cases");
+                yield return new TestCaseData(new Stack<int>(new[] { 3, 2, 1 })).Returns(new Stack<int>(new[] { 1, 2, 3 })).SetName("Some cases");
+                yield return new TestCaseData(new Stack<int>(new[] { 3, 1, 2 })).Returns(new Stack<int>(new[] { 1, 2, 3 })).SetName("Some cases");
+                yield return new TestCaseData(new Stack<int>()).Returns(new Stack<int>()).SetName("Some cases");                                                
+            }
+        }
+    }
+}
---
+++ CrackingCodingInterview/Chapter3/SortStackAscOrder.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CrackingCodingInterview.Chapter3
+{
+    [Reference(
+        Page = 52
+        , Number = "3.6"
+        , Description = "Write a program to sort a stack in ascending order. " +
+                        "You should not make any assumptions about how the stack is implemented." +
+                        "The following are the only functions that should be used to write" +
+                        "this program: push|pop|peek|isEmpty")]
+    public class SortStackAscOrder : Algorithm<Stack<int>, Stack<int>>
+    {
+        protected override int OnInitComplexity(Stack<int> arg)
+        {
+            if (arg == null) throw new ArgumentNullException("arg");
+            return arg.Count;
+        }
+        
+        protected override Stack<int> OnExecute(Stack<int> arg)
+        {
+            if (arg == null) throw new ArgumentNullException("arg");
+
+            var stack = new Stack<int>(arg);
+            arg.Clear();
+
+            while (stack.Count > 0)
+            {
+                IncrementIteration();
+
+                var tmp = stack.Pop();
+
+                while ((arg.Count > 0) && (arg.Peek() > tmp))
+                {
+                    IncrementIteration();
+                    stack.Push(arg.Pop());
+                }
+
+                arg.Push(tmp);
+            }
+
+            return arg;
+        }
+    }
+}
