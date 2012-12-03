330c3d3dd828cfc581c7f633318e516a4b16ccd8
Oleksandr Lukiantsev <alexander.lukyantsev@gmail.com>
2012-12-02 18:16:44 -0800

    added #4.6

---
+++ CrackingCodingInterview.Test/Chapter4/FindCommonAncestorTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using CrackingCodingInterview.Chapter4;
+using NUnit.Framework;
+
+namespace CrackingCodingInterview.Test.Chapter4
+{
+    [TestFixture]
+    public class FindCommonAncestorTest : TestBase
+    {
+        public static IEnumerable TestCases
+        {
+            get
+            {
+                var a = new TreeNode<int> { Value = 1 };
+                var b = new TreeNode<int> { Value = 2 };
+                var c = new TreeNode<int> { Value = 3 };
+                var d = new TreeNode<int> { Value = 4 };
+                var e = new TreeNode<int> { Value = 5 };
+                var f = new TreeNode<int> { Value = 6 };
+                var g = new TreeNode<int> { Value = 7 };
+                var h = new TreeNode<int> { Value = 8 };
+                var i = new TreeNode<int> { Value = 9 };
+
+                f.Left = b;
+                f.Right = g;
+                b.Left = a;
+                b.Right = d;
+                d.Left = c;
+                d.Right = e;
+                g.Right = i;
+                i.Left = h;
+
+                yield return new TestCaseData(new[] { f, c, e }).Returns(d).SetName("d");
+                yield return new TestCaseData(new[] { f, e, h }).Returns(f).SetName("f");
+                yield return new TestCaseData(new[] { f, a, d }).Returns(b).SetName("b");
+                yield return new TestCaseData(new[] { f, a, h }).Returns(f).SetName("f2");
+                yield return new TestCaseData(new[] { f, a, b }).Returns(f).SetName("f3");
+                yield return new TestCaseData(new[] { f, b, h }).Returns(f).SetName("f4");
+
+                
+            }
+        }
+
+        [Test]
+        [TestCaseSource("TestCases")]
+        public TreeNode<int> Test(TreeNode<int> root, TreeNode<int> first, TreeNode<int> second)
+        {
+            var tuple = Tuple.Create(root, first, second);
+            return RunTest(tuple, new FindCommonAncestor<int>());
+        }
+    }
+}
---
+++ CrackingCodingInterview/Chapter4/FindCommonAncestor.cs
@@ -0,0 +1,139 @@
+﻿using System;
+
+namespace CrackingCodingInterview.Chapter4
+{
+
+    [Reference(
+        Page = 54
+        , Number = "4.6"
+        , Description = "Dsign algorithm and write code too find the first common ancestor of two nodes" +
+                        "in a binary tree. Avoid  storing additional nodes in a data structure. NOTE: This is not" +
+                        "necessarily a binary search tree")]
+
+    public class FindCommonAncestor<T> : Algorithm<Tuple<TreeNode<T>, TreeNode<T>, TreeNode<T>>, TreeNode<T>>
+    {
+        protected override TreeNode<T> OnExecute(Tuple<TreeNode<T>, TreeNode<T>, TreeNode<T>> arg)
+        {
+            if (arg == null)
+            {
+                throw new ArgumentNullException("arg");
+            }
+
+            TreeNode<T> root = arg.Item1;
+            if (root == null)
+            {
+                throw new ArgumentException("arg.Item1 should not be null");
+            }
+            TreeNode<T> firstNode = arg.Item2;
+            if (firstNode == null)
+            {
+                throw new ArgumentException("arg.Item2 should not be null");
+            }
+            TreeNode<T> secondNode = arg.Item3;
+            if (secondNode == null)
+            {
+                throw new ArgumentException("arg.Item3 should not be null");
+            }
+
+            return GetCommonAncestor(root, firstNode, secondNode);
+        }
+
+        private static TreeNode<T> GetCommonAncestor(TreeNode<T> root, TreeNode<T> firstNode, TreeNode<T> secondNode)
+        {
+            TreeNode<T> left = root.Left;
+            TreeNode<T> right = root.Right;
+
+            if ((firstNode == secondNode) && ((left == firstNode) || (right == firstNode)))
+            {
+                return root;
+            }
+
+            NodesFound foundNodesFromLeft = GetChildNodesFoundForRoot(left, firstNode, secondNode);
+
+            if (foundNodesFromLeft == NodesFound.Two)
+            {
+                if ((left == firstNode) || (left == secondNode))
+                {
+                    return root;
+                }
+                
+                return GetCommonAncestor(left, firstNode, secondNode);
+            }
+            
+            if (foundNodesFromLeft == NodesFound.One)
+            {
+                if (root == firstNode)
+                {
+                    return firstNode;
+                }
+                if (root == secondNode)
+                {
+                    return secondNode;
+                }
+            }
+
+            NodesFound foundNodesFromRight = GetChildNodesFoundForRoot(right, firstNode, secondNode);
+
+            if (foundNodesFromRight == NodesFound.Two)
+            {
+                if ((right == firstNode) || (right == secondNode))
+                {
+                    return root;
+                }
+
+                return GetCommonAncestor(right, firstNode, secondNode);
+            }
+
+            if (foundNodesFromRight == NodesFound.One)
+            {
+                if (root == firstNode)
+                {
+                    return firstNode;
+                }
+
+                if (root == secondNode)
+                {
+                    return secondNode;
+                }
+            }
+
+            if ((foundNodesFromLeft == NodesFound.One) && (foundNodesFromRight == NodesFound.One))
+            {
+                return root;
+            }
+            
+            return null;
+        }
+
+        private static NodesFound GetChildNodesFoundForRoot(TreeNode<T> root, TreeNode<T> first, TreeNode<T> second)
+        {
+            var nodesFound = NodesFound.None;
+            
+            if (root == null)
+            {
+                return nodesFound;
+            }
+
+            if ((root == first) || (root == second))
+            {
+                nodesFound++;
+            }
+
+            nodesFound += (byte)GetChildNodesFoundForRoot(root.Left, first, second);
+
+            if (nodesFound == NodesFound.Two)
+            {
+                return nodesFound;
+            }
+
+            return nodesFound + (byte)GetChildNodesFoundForRoot(root.Right, first, second);
+        }
+
+        enum NodesFound : byte
+        {
+            None = 0,
+            One = 1,
+            Two = 2
+        }
+    }
+}
