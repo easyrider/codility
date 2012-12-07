83f2693967cf13cb897d815a3559363bb1d0c1b4
Oleksandr Lukiantsev <alexander.lukyantsev@gmail.com>
2012-12-06 22:10:03 -0800

    added #4.8

---
+++ CrackingCodingInterview.Test/Chapter4/FindSumPathsInTreesTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using CrackingCodingInterview.Chapter4;
+using NUnit.Framework;
+
+namespace CrackingCodingInterview.Test.Chapter4
+{
+    [TestFixture]
+    public class FindSumPathsInTreesTest : TestBase
+    {
+        private static IEnumerable TestCases
+        {
+            get
+            {
+                var a = new TreeNode<int> { Value = 1 };
+                var b = new TreeNode<int> { Value = 2 };
+                var c = new TreeNode<int> { Value = 3 };
+                var d = new TreeNode<int> { Value = 4 };
+                var e = new TreeNode<int> { Value = 5 };
+                var f = new TreeNode<int> { Value = 6 };
+                var g = new TreeNode<int> { Value = 7 };
+                var h = new TreeNode<int> { Value = 8 };
+                var i = new TreeNode<int> { Value = 9 };
+
+                f.Left = b;
+                f.Right = g;
+                b.Left = a;
+                b.Right = d;
+                d.Left = c;
+                d.Right = e;
+                g.Right = i;
+                i.Left = h;
+
+                yield return new TestCaseData(f, 1).Returns("[1];").SetName("1");
+                yield return new TestCaseData(f, 2).Returns("[2];").SetName("2");
+                yield return new TestCaseData(f, 4).Returns("[4];").SetName("4");
+                yield return new TestCaseData(f, 8).Returns("[6][2];[8];").SetName("8");
+                yield return new TestCaseData(f, 16).Returns("[7][9];").SetName("16");
+                yield return new TestCaseData(f, 32).Returns("").SetName("32");
+            }
+        }
+
+        [Test]
+        [TestCaseSource("TestCases")]
+        public string Test(TreeNode<int> root, int sum)
+        {
+            Tuple<TreeNode<int>, int> tuple = Tuple.Create(root, sum);
+            return RunTest(tuple, new FindSumPathsInTree());
+        }
+    }
+}
---
+++ CrackingCodingInterview/Chapter4/FindSumPathsInTree.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrackingCodingInterview.Chapter4
+{
+    [Reference(
+        Page = 54
+        , Number = "4.8"
+        , Description = "You are given a binary tree in which each node contains a value. Design an algorithm" +
+                        "to print all paths which sum up to that value. Note that it can be any path in the tree" +
+                        "- it does not have to start at the root")]
+    public class FindSumPathsInTree : Algorithm<Tuple<TreeNode<int>, int>, string>
+    {
+        protected override string OnExecute(Tuple<TreeNode<int>, int> arg)
+        {
+            if (arg == null)
+            {
+                throw new ArgumentNullException("arg");
+            }
+
+            TreeNode<int> treeNode = arg.Item1;
+            
+            if (treeNode == null)
+            {
+                throw new ArgumentException("Item1", "arg");
+            }
+
+            int sum = arg.Item2;
+
+            return FindSum(treeNode, sum, new List<int>(),  0);
+
+        }
+
+        private static string FindSum(TreeNode<int> treeNode, int sum, List<int> buffer, int level)
+        {
+            if (treeNode == null)
+            {
+                return string.Empty;
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            int tmp = sum;
+            buffer.Add(treeNode.Value);
+            for (int i = level; i > -1; i--)
+            {
+                tmp -= buffer[i];
+                if (tmp == 0)
+                {
+                    sb.Append(Print(buffer, i, level));
+                }
+            }
+
+            List<int> copy1 = new List<int>(buffer);
+            List<int> copy2 = new List<int>(buffer);
+            level++;
+            sb.Append(FindSum(treeNode.Left, sum, copy1, level));
+            sb.Append(FindSum(treeNode.Right, sum, copy2, level));
+
+            return sb.ToString();
+        }
+
+        private static string Print(IList<int> buffer, int level, int i2)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = level; i <= i2; i++)
+            {
+                sb.AppendFormat("[{0}]", buffer[i]);
+            }
+
+            sb.Append(";");
+
+            return sb.ToString();
+        }
+    }
+}
