2e46f711681b5399b9218e1fceb924ebc2cb763d
Oleksandr Lukiantsev <alexander.lukyantsev@gmail.com>
2012-12-08 20:59:21 -0800

    added #5.1

---
+++ CrackingCodingInterview.Test/Chapter5/SetBitsTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using CrackingCodingInterview.Chapter5;
+using NUnit.Framework;
+
+namespace CrackingCodingInterview.Test.Chapter5
+{
+    [TestFixture]
+    public class SetBitsTest : TestBase
+    {
+        private static IEnumerable TestCases
+        {
+            get
+            {
+                yield return new TestCaseData("10000000000", "10101", 2, 6)
+                    .Returns("10001010100")
+                    .SetName("Example from the book");
+
+                yield return new TestCaseData("10000000011", "110", 1, 3)
+                     .Returns("10000001101")
+                     .SetName("My example");
+
+            }
+        }
+
+        [Test]
+        [TestCaseSource("TestCases")]
+        public string Test(string n, string m, int i, int j)
+        {
+            var result = RunTest(Tuple.Create(Convert.ToInt32(n, 2), Convert.ToInt32(m, 2), i, j), new SetBits());
+            return Convert.ToString(result, 2);
+        }
+}
+}
---
+++ CrackingCodingInterview/Chapter5/SetBits.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CrackingCodingInterview.Chapter5
+{
+     [Reference(
+        Page = 58
+        , Number = "5.1"
+        , Description = "You are given two 32-bit numbers, N and M, and two bit positions, i and j. Write a " +
+                        "method to set all bits between i and j in N equal to M(e.g., M becomes a substring of" +
+                        "N located at i and starting at j.")]
+    public class SetBits : Algorithm<Tuple<int, int, int, int>, int>
+    {
+        protected override int OnExecute(Tuple<int, int, int, int> arg)
+        {
+            if (arg == null)
+            {
+                throw new ArgumentNullException("arg");
+            }
+
+            int n = arg.Item1;
+            int m = arg.Item2;
+            int i = arg.Item3;
+            int j = arg.Item4;
+
+            const int allOnes = ~0;
+            int left = allOnes -((1 << j) - 1);
+            int right = ((1 << i) - 1);
+            int mask = left | right;
+            return (n & mask) | (m << i);
+        }
+    }
+}
