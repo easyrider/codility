8a98014e517cd88160a29c406fca346934291b3b
Oleksandr Lukiantsev <alexander.lukyantsev@gmail.com>
2012-12-11 22:48:27 -0800

    added #5.3

---
+++ CrackingCodingInterview.Test/Chapter5/GetNextSmallerAndBiggerNumsWithSameAmountOfBitsTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Diagnostics;
+using CrackingCodingInterview.Chapter5;
+using NUnit.Framework;
+
+namespace CrackingCodingInterview.Test.Chapter5
+{
+
+    [TestFixture]
+    public class GetNextSmallerAndBiggerNumsWithSameAmountOfBitsTest : TestBase
+    {
+        private static IEnumerable TestCases
+        {
+            get
+            {
+                yield return new TestCaseData((uint)10);
+                yield return new TestCaseData((uint)33);
+                yield return new TestCaseData((uint)57);
+                yield return new TestCaseData((uint)99);
+
+            }
+        }
+
+        [Test]
+        [TestCaseSource("TestCases")]
+        public void Test(uint num)
+        {
+            var result = RunTest(num, new GetNextSmallerAndBiggerNumsWithSameAmountOfBits());
+            Trace.TraceInformation("num: b{0} => {1}", Convert.ToString(num, 2), num);
+            Trace.TraceInformation("result.Item1: b{0} => {1}", Convert.ToString(result.Item1, 2), result.Item1);
+            Trace.TraceInformation("result.Item2: b{0} => {1}", Convert.ToString(result.Item2, 2), result.Item2);
+        }
+    }
+}
---
+++ CrackingCodingInterview/Chapter5/GetNextSmallerAndBiggerNumsWithSameAmountOfBits.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace CrackingCodingInterview.Chapter5
+{
+     [Reference(
+        Page = 58
+        , Number = "5.3"
+        , Description = "Given an integer, print the next smallest and next largest number that have the same" +
+                        "number of 1 bits in their binary representation.")]
+    public class GetNextSmallerAndBiggerNumsWithSameAmountOfBits : Algorithm<uint, Tuple<uint, uint>>
+    {
+        protected override Tuple<uint, uint> OnExecute(uint arg)
+        {
+            return Tuple.Create(GetSmaller(arg), GetBigger(arg));
+        }
+
+        private static uint GetBigger(uint u)
+        {
+            return GetNext(u, true);
+        }
+
+        private static uint GetNext(uint arg, bool bigger)
+        {
+            if (arg == 0)
+            {
+              return 0;
+            }
+
+            int index = 0;
+            int count = 0;
+
+            while ((!GetBit(arg, index) && bigger) || (GetBit(arg, index) && !bigger))
+            {
+                index++;
+            }
+
+            while ((GetBit(arg, index) && bigger) || (!GetBit(arg, index) && !bigger))
+            {
+                index++;
+                count++;
+            }
+
+            arg = SetBit(arg, index, bigger);
+            index--;
+            arg = SetBit(arg, index, !bigger);
+            count--;
+
+            for (int i = index - 1; i >= count; i--)
+            {
+                arg = SetBit(arg, i, !bigger);
+            }
+
+            for (int i = count - 1; i >= 0; i--)
+            {
+                arg = SetBit(arg, i, bigger);
+            }
+
+            return arg;
+        }
+
+        private static uint GetSmaller(uint arg)
+        {
+            return GetNext(arg, false);
+        }
+
+        private static bool GetBit(uint u, int index)
+        {
+            return ((u & (1 << index)) > 0);
+        }
+
+        private static uint SetBit(uint u, int index, bool bit)
+        {
+            if (bit)
+            {
+                return u | (uint) (1 << index);
+            }
+            
+            uint mask = (uint)~(1 << index);
+            return u & mask;
+        }
+    }
+}
