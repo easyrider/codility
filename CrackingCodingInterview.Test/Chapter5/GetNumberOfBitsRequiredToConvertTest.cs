40dcb08ed96cc6c66e968a96557d35e7d694d49d
Oleksandr Lukiantsev <alexander.lukyantsev@gmail.com>
2012-12-14 18:26:45 -0800

    added #5.5

---
+++ CrackingCodingInterview.Test/Chapter5/GetNumberOfBitsRequiredToConvertTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Diagnostics;
+using CrackingCodingInterview.Chapter5;
+using NUnit.Framework;
+
+namespace CrackingCodingInterview.Test.Chapter5
+{
+    [TestFixture]
+    public class GetNumberOfBitsRequiredToConvertTest : TestBase
+    {
+        private static IEnumerable TestCases
+        {
+            get
+            {
+                yield return new TestCaseData(2, 24).Returns(3);
+                yield return new TestCaseData(3, 140).Returns(5);
+                yield return new TestCaseData(22, 23).Returns(1);
+                yield return new TestCaseData(8, 5).Returns(3);
+            }
+        }
+
+        [Test]
+        [TestCaseSource("TestCases")]
+        public int Test(int a, int b)
+        {
+            return RunTest(Tuple.Create(a, b), new GetNumberOfBitsRequiredToConvert());
+        }
+    }
+}
---
+++ CrackingCodingInterview/Chapter5/GetNumberOfBitsRequiredToConvert.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CrackingCodingInterview.Chapter5
+{
+    [Reference(
+        Page = 58
+        , Number = "5.5"
+        , Description = "Write a function to determine the number of bits required to convert integer A to integer B")]
+    public class GetNumberOfBitsRequiredToConvert : Algorithm<Tuple<int, int>, int>
+    {
+        protected override int OnExecute(Tuple<int, int> arg)
+        {
+            if (arg == null)
+            {
+                throw new ArgumentNullException("arg");
+            }
+
+            int a = arg.Item1;
+            int b = arg.Item2;
+            int count = 0;
+
+            for (int i = a ^ b; i != 0x00; i >>= 1)
+            {
+                count += i & 0x01;
+            }
+
+            return count;
+        }
+    }
+}
