77e212e69c0b94e81dc7d75b646b0eb030431b7e
Oleksandr Lukiantsev <alexander.lukyantsev@gmail.com>
2012-12-15 23:00:27 -0800

    added #5.6

---
+++ CrackingCodingInterview.Test/Chapter5/SwapOddAndEventBitsTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using CrackingCodingInterview.Chapter5;
+using NUnit.Framework;
+
+namespace CrackingCodingInterview.Test.Chapter5
+{
+    [TestFixture]
+    public class SwapOddAndEventBitsTest : TestBase
+    {
+        private static IEnumerable TestData
+        {
+            get
+            {
+                yield return new TestCaseData("0101").Returns("1010");
+                yield return new TestCaseData("1010").Returns("0101");
+                yield return new TestCaseData("1100").Returns("1100");
+            }
+        }
+
+        
+        [Test]
+        [TestCaseSource("TestData")]
+        public string Test(string arg)
+        {
+            uint input = Convert.ToUInt32(arg, 2);
+            uint result = RunTest(input, new SwapOddAndEventBits());
+            return Convert.ToString(result, 2).PadLeft(arg.Length, '0');
+        }
+    }
+}
---
+++ CrackingCodingInterview/Chapter5/SwapOddAndEventBits.cs
@@ -0,0 +1,17 @@
+﻿namespace CrackingCodingInterview.Chapter5
+{
+    [Reference(
+        Page = 58
+        , Number = "5.6"
+        , Description = "Write a program to swap odd and even bits in an integer with as few instructions as" +
+                        "possible")]
+    public class SwapOddAndEventBits : Algorithm<uint, uint>
+    {
+        protected override uint OnExecute(uint arg)
+        {
+            const uint oddBitsMask = 0xaaaaaaaa;
+            const uint evenBitsMask = 0x55555555;
+            return (((arg & oddBitsMask) >> 1) | ((arg & evenBitsMask) << 1));
+        }
+    }
+}
