57b893cbf25602b7d6a7a3a310563aee63157ce3
Oleksandr Lukiantsev <alexander.lukyantsev@gmail.com>
2012-12-17 23:54:08 -0800

    added #5.7

---
+++ CrackingCodingInterview.Test/Chapter5/FindMissingNumbersTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using CrackingCodingInterview.Chapter5;
+using NUnit.Framework;
+
+namespace CrackingCodingInterview.Test.Chapter5
+{
+
+    [TestFixture]
+    public class FindMissingNumbersTest : TestBase
+    {
+        private static IEnumerable TestCases
+        {
+            get
+            {
+                yield return new TestCaseData(new[] {0, 1, 2, 3, 4, 5, 7, 8, 9})
+                    .Returns(6);
+            }
+        }
+
+        [Test]
+        [TestCaseSource("TestCases")]
+        public int Test(int[] args)
+        {
+            return RunTest(args, new FindMissingNumbers());
+        }
+    }
+}
---
+++ CrackingCodingInterview/Chapter5/FindMissingNumbers.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+
+namespace CrackingCodingInterview.Chapter5
+{
+    [Reference(
+        Page = 58
+        , Number = "5.7"
+        , Description = "An array [1...n] contains all the integers from 0 to n except for one number which is" +
+                        "missing. In this problem, we cannot access an entire integer in A with a single operation." +
+                        "The elements of A are represented in binary, and the only operation we can use to access" +
+                        "them is 'fetch the jth bit of A[i]', which takes constant time. Write code to find the missing integer." +
+                        "Can you do it in O(n) time?")]
+    public class FindMissingNumbers : Algorithm<int[], int>
+    {
+        protected override int OnExecute(int[] arg)
+        {
+            return FindMissing(arg, sizeof (int) * 8 - 1);
+        }
+
+        private static int FindMissing(IEnumerable<int> ints, int position)
+        {
+            if (position < 0)
+            {
+                return 0;
+            }
+
+            List<int> odds = new List<int>();
+            List<int> evens = new List<int>();
+
+            uint mask = (0x80000000 >> position);
+            
+            foreach (int i in ints)
+            {   
+                if ((i & mask) == 0)
+                {
+                    evens.Add(i);
+                }
+                else
+                {
+                    odds.Add(i);
+                }
+            }
+
+            if (odds.Count >= evens.Count)
+            {
+                return FindMissing(evens, position - 1) << 1 | 0;
+            }
+            
+            return FindMissing(odds, position - 1) << 1 | 1;
+        }
+    }
+}
