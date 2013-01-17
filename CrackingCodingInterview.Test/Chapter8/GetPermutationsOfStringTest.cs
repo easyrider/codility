05a118aec0b4e7c19a83ee8d3ae9d54da5937e29
Oleksandr Lukiantsev <alexander.lukyantsev@gmail.com>
2013-01-16 21:02:19 -0800

    Added #8.4

---
+++ CrackingCodingInterview.Test/Chapter8/GetPermutationsOfStringTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using CrackingCodingInterview.Chapter8;
+using NUnit.Framework;
+
+namespace CrackingCodingInterview.Test.Chapter8
+{
+
+    [TestFixture]
+    public class GetPermutationsOfStringTest : TestBase
+    {
+        [Test]
+        public void Test()
+        {
+            const string originalString = "abc";
+            List<string> expectedPermuted = new List<string>
+                                                {
+                                                    "abc",
+                                                    "bac",
+                                                    "bca",
+                                                    "acb",
+                                                    "cab",
+                                                    "cba"
+                                                };
+
+            GetPermutationsOfString getPermutationsOfString = new GetPermutationsOfString();
+            List<string> permuted = getPermutationsOfString.Execute(originalString);
+            CollectionAssert.AreEquivalent(expectedPermuted, permuted);
+        }
+    }
+}
---
+++ CrackingCodingInterview/Chapter8/GetPermutationsOfString.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CrackingCodingInterview.Chapter8
+{
+    [Reference(
+        Page = 64
+        , Number = "8.4"
+        , Description = "Write a method to compute all permutations of a string.")]
+    public class GetPermutationsOfString : Algorithm<string, List<string>>
+    {
+        protected override List<string> OnExecute(string arg)
+        {
+            if (arg == null)
+            {
+                throw new ArgumentNullException("arg");
+            }
+
+            List<string> permutations = new List<string>();
+
+            if (arg.Length == 0)
+            {
+                permutations.Add(string.Empty);
+                return permutations;
+            }
+
+            string firstChar = arg.Substring(0, 1);
+            string reminder = arg.Substring(1);
+
+            List<string> words = OnExecute(reminder);
+
+            foreach (string word in words)
+            {
+                for (int i = 0; i <= word.Length; i++)
+                {
+                    permutations.Add(word.Insert(i,firstChar));
+                }
+            }
+
+            return permutations;
+        }
+    }
+}
