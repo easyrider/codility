0ddca09fb2738c8af8d9793aef60a3aa9dea512c
Oleksandr Lukiantsev <alexander.lukyantsev@gmail.com>
2013-01-23 22:21:54 -0800

    Added #8.6

---
+++ CrackingCodingInterview.Test/Chapter8/PaintFillTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using CrackingCodingInterview.Chapter8;
+using NUnit.Framework;
+
+namespace CrackingCodingInterview.Test.Chapter8
+{
+    [TestFixture]
+    public class PaintFillTest : TestBase
+    {
+        [Test]
+        public void Test()
+        {
+            PaintFill paintFill = new PaintFill();
+            Color[,] picture = new Color[3,3];
+
+            picture[0, 0] = Color.Red;
+            picture[0, 1] = Color.Green;
+            picture[0, 2] = Color.Red;
+
+            picture[1, 0] = Color.Green;
+            picture[1, 1] = Color.Green;
+            picture[1, 2] = Color.Blue;
+
+            picture[2, 0] = Color.Blue;
+            picture[2, 1] = Color.Red;
+            picture[2, 2] = Color.Green;
+            
+            paintFill.Execute(Tuple.Create(picture, 1, 1, Color.Red));
+
+            Assert.AreEqual(Color.Red, picture[0, 0]);
+            Assert.AreEqual(Color.Red, picture[0, 1]);
+            Assert.AreEqual(Color.Red, picture[0, 2]);
+
+            Assert.AreEqual(Color.Red, picture[1, 0]);
+            Assert.AreEqual(Color.Red, picture[1, 1]);
+            Assert.AreEqual(Color.Blue, picture[1, 2]);
+
+            Assert.AreEqual(Color.Blue, picture[2, 0]);
+            Assert.AreEqual(Color.Red, picture[2, 1]);
+            Assert.AreEqual(Color.Green, picture[2, 2]);
+        }
+    }
+}
---
+++ CrackingCodingInterview/Chapter8/PaintFill.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace CrackingCodingInterview.Chapter8
+{
+    [Reference(
+        Page = 64
+        , Number = "8.6"
+        , Description = "Implement the 'paint fill' function that one might see on many image editing programs." +
+                        "That is, given a screen (represented by a 2-dimensional array of Colors), a" +
+                        "point, and a new color, fill in the surrounding area until you hit a border of that color")]
+    public class PaintFill : Algorithm<Tuple<Color[,], int, int, Color>, Color[,]>
+    {
+        protected override Color[,] OnExecute(Tuple<Color[,], int, int, Color> arg)
+        {
+            Color[,] picture = arg.Item1;
+            int x = arg.Item2;
+            int y = arg.Item3;
+            Color newColor = arg.Item4;
+
+            Fill(picture, x, y, picture[x, y], newColor);
+
+            return picture;
+        }
+
+        private static void Fill(Color[,] picture, int x, int y, Color oldColor, Color newColor)
+        {
+            if (x < 0 
+                || x >= picture.GetLength(0) 
+                || y < 0 
+                || y >= picture.GetLength(1)
+                || picture[x, y] != oldColor)
+            {
+                return;
+            }
+            
+            picture[x, y] = newColor;
+            Fill(picture, x - 1, y, oldColor, newColor);
+            Fill(picture, x + 1, y, oldColor, newColor);
+            Fill(picture, x, y - 1, oldColor, newColor);
+            Fill(picture, x, y + 1, oldColor, newColor);
+        }
+    }
+
+    public enum Color
+    {
+        Red, 
+        Green,
+        Blue
+    }
+}
