822dc508138b6e8600728f97d8819ba42115acf7
Oleksandr Lukiantsev <oleksandr.lukiantsev@gmail.com>
2013-03-10 18:35:14 -0700

    added #9.1

---
+++ CrackingCodingInterview.Test/Chapter9/MergeTwoSortedArraysInSortedOrderTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using CrackingCodingInterview.Chapter9;
+using NUnit.Framework;
+
+namespace CrackingCodingInterview.Test.Chapter9
+{
+    [TestFixture]
+    public class MergeTwoSortedArraysInSortedOrderTest : TestBase
+    {
+        private static IEnumerable TestCases
+        {
+            get
+            {
+                yield return
+                    new TestCaseData(new int[] {4, 5, 6}, new int[] {1, 2, 3}).Returns(new int[] {1, 2, 3, 4, 5, 6});
+                yield return
+                    new TestCaseData(new int[] { 1, 3, 6 }, new int[] { 2, 4, 5, 7 }).Returns(new int[] { 1, 2, 3, 4, 5, 6, 7 });
+
+            }
+        }
+
+
+        [TestCaseSource("TestCases")]
+        [Test]
+        public int[] Test(int[] arrayA, int[] arrayB)
+        {
+            int[] result = RunTest(Tuple.Create(arrayA, arrayB), new MergeTwoSortedArraysInSortedOrder());
+            return result;
+        }
+    }
+}
---
+++ CrackingCodingInterview/Chapter9/MergeTwoSortedArraysInSortedOrder.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace CrackingCodingInterview.Chapter9
+{
+    [Reference(
+        Page = 66
+        , Number = "9.1"
+        , Description = "You are given two sorted arrats, write a method to merge one array into another in sorted manner"
+        )]
+    public class MergeTwoSortedArraysInSortedOrder : Algorithm<Tuple<int[], int[]>, int[]>
+    {
+        protected override int[] OnExecute(Tuple<int[], int[]> arg)
+        {
+            int[] arrayA = arg.Item1;
+            int[] arrayB = arg.Item2;
+            int[] arrayResult = new int[arrayA.Length + arrayB.Length];
+
+            int arrayResultPointer = arrayResult.Length - 1;
+            int arrayAPointer = arrayA.Length - 1;
+            int arrayBPointer = arrayB.Length - 1;
+
+            while (arrayAPointer >= 0 && arrayBPointer >= 0)
+            {
+                if (arrayA[arrayAPointer] > arrayB[arrayBPointer])
+                {
+                    arrayResult[arrayResultPointer--] = arrayA[arrayAPointer--];
+                }
+                else
+                {
+                    arrayResult[arrayResultPointer--] = arrayB[arrayBPointer--];
+                }
+            }
+
+            while (arrayBPointer >= 0)
+            {
+                arrayResult[arrayResultPointer--] = arrayB[arrayBPointer--];
+            }
+
+            while (arrayAPointer >= 0)
+            {
+                arrayResult[arrayResultPointer--] = arrayA[arrayAPointer--];
+            }
+
+            return arrayResult;
+        }
+    }
+}
