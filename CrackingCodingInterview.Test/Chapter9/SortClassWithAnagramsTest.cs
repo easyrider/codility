5ef5b44b89c9aa456c0357d7c97481f12e6732d1
Oleksandr Lukiantsev <oleksandr.lukiantsev@gmail.com>
2013-03-11 21:22:46 -0700

    added #9.2

---
+++ CrackingCodingInterview.Test/Chapter9/SortClassWithAnagramsTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using CrackingCodingInterview.Chapter9;
+using NUnit.Framework;
+
+namespace CrackingCodingInterview.Test.Chapter9
+{
+    [TestFixture]
+    public class SortClassWithAnagramsTest : TestBase
+    {
+        private static IEnumerable TestCases
+        {
+            get
+            {
+                yield return
+                    new TestCaseData((object)new[] { "foo", "rba", "bar", "baz", "zab", "ofo" })
+                    .Returns(new string[] { "rba", "bar", "baz", "zab", "ofo", "foo"});
+                
+            }
+        }
+
+
+        [TestCaseSource("TestCases")]
+        [Test]
+        public string[] Test(string[] array)
+        {
+            string[] result = RunTest(array, new SortClassWithAnagrams());
+            return result;
+        }
+    }
+}
---
+++ CrackingCodingInterview/Chapter9/SortClassWithAnagrams.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CrackingCodingInterview.Chapter9
+{
+
+    [Reference(
+        Page = 66
+        , Number = "9.2"
+        , Description = "Write an algorithm to sort an array of strings so that all anagrams are next to each other."
+        )]
+    public class SortClassWithAnagrams : Algorithm<string[], string[]>
+    {
+        protected override string[] OnExecute(string[] arg)
+        {
+            Array.Sort(arg, new AnagramComparer());
+            return arg;
+        }
+
+        class AnagramComparer : IComparer<string>
+        {
+            public int Compare(string x, string y)
+            {
+                return String.Compare(SortString(x), SortString(y), StringComparison.Ordinal);
+            }
+
+            private static string SortString(string str)
+            {
+                char[] charArray = str.ToCharArray();
+                Array.Sort(charArray);
+                return new string(charArray);
+            }
+        }
+    }
+}
