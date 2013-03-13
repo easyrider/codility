6eab76b098b030f1ee6672dd3ce86bd59bb27d57
Oleksandr Lukiantsev <oleksandr.lukiantsev@gmail.com>
2013-03-13 13:17:52 -0700

    added  #9.3

---
+++ CrackingCodingInterview.Test/Chapter9/FindIndexOfElementInSortedRotatedArrayTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using CrackingCodingInterview.Chapter9;
+using NUnit.Framework;
+
+namespace CrackingCodingInterview.Test.Chapter9
+{
+    [TestFixture]
+    public class FindIndexOfElementInSortedRotatedArrayTest : TestBase
+    {
+        private static IEnumerable TestCases
+        {
+            get
+            {
+                yield return
+                    new TestCaseData(new int[] {15, 16, 19, 20, 25, 1, 3, 4, 5, 7, 10, 14}, 5).Returns(8);
+            }
+        }
+
+
+        [TestCaseSource("TestCases")]
+        [Test]
+        public int Test(int[] array, int value)
+        {
+            int result = RunTest(Tuple.Create(array, value), new FindIndexOfElementInSortedRotatedArray());
+            return result;
+        }
+    }
+}
---
+++ CrackingCodingInterview/Chapter9/FindIndexOfElementInSortedRotatedArray.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CrackingCodingInterview.Chapter9
+{
+    [Reference(
+        Page = 66
+        , Number = "9.3"
+        , Description = "Given a sorted array of n integers that has been rotated an unknown number of times, give an O(log n) " +
+                        "algorithm that finds an element in the array. You may assume that the array was originally sorted in increasing order."
+        )]
+    public class FindIndexOfElementInSortedRotatedArray : Algorithm<Tuple<int[], int>, int>
+    {
+        protected override int OnExecute(Tuple<int[], int> arg)
+        {
+            int[] array = arg.Item1;
+            return Search(arg.Item2, array, 0, array.Length - 1);
+        }
+
+        static int Search(int value, int[] inArray, int from, int to)
+        {
+            while (from <= to)
+            {
+                int mean = (from + to) / 2;
+
+                if (value == inArray[mean])
+                {
+                    return mean;
+                }
+                else if(inArray[from] <= inArray[mean])
+                {
+                    if (value > inArray[mean])
+                    {
+                        from = mean + 1;
+                    }
+                    else if (value >= inArray[from])
+                    {
+                        to = mean - 1;
+                    }
+                    else
+                    {
+                        from = mean + 1;
+                    }
+                }
+                else if (value < inArray[mean])
+                {
+                    to = mean - 1;
+                }
+                else if (value <= inArray[to])
+                {
+                    from = mean + 1;
+                }
+                else
+                {
+                    to = mean - 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
