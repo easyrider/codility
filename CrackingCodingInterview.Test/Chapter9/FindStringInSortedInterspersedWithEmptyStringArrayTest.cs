1e6486b12a2ad4a63cb4f883af7b7654711a4088
Oleksandr Lukiantsev <oleksandr.lukiantsev@gmail.com>
2013-03-15 16:55:31 -0700

    added #9.5

---
+++ CrackingCodingInterview.Test/Chapter9/FindStringInSortedInterspersedWithEmptyStringArrayTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using CrackingCodingInterview.Chapter9;
+using NUnit.Framework;
+
+namespace CrackingCodingInterview.Test.Chapter9
+{
+    [TestFixture]
+    public class FindStringInSortedInterspersedWithEmptyStringArrayTest : TestBase
+    {
+        private static IEnumerable TestCases
+        {
+            get
+            {
+                yield return
+                    new TestCaseData(new string[] {"at", "", "", "", "ball", "", "", "car", "", "", "dad", "", ""}, "ball")
+                    .Returns(4);
+                yield return
+                    new TestCaseData(new string[] { "at", "", "", "", "", "ball", "car", "", "", "dad", "", "" }, "ballcar")
+                        .Returns(-1);
+            }
+        }
+
+
+        [TestCaseSource("TestCases")]
+        [Test]
+        public int Test(string[] array, string value)
+        {
+            int result = RunTest(Tuple.Create(array, value), new FindStringInSortedInterspersedWithEmptyStringArray());
+            return result;
+        }
+    }
+}
---
+++ CrackingCodingInterview/Chapter9/FindStringInSortedInterspersedWithEmptyStringArray.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CrackingCodingInterview.Chapter9
+{
+     [Reference(
+        Page = 66
+        , Number = "9.5"
+        , Description = "Given a sorted array of strings which is interspersed with empty strings, " +
+                        "write a method to find the location of a given string."
+        )]
+    public class FindStringInSortedInterspersedWithEmptyStringArray : Algorithm<Tuple<String[], string>, int>
+    {
+        protected override int OnExecute(Tuple<string[], string> arg)
+        {
+            String[] array = arg.Item1;
+            string value = arg.Item2;
+
+            if ((array == null) || (value == null))
+            {
+                return -1;
+            }
+
+            if (value == string.Empty)
+            {
+                return SearchLinear(array, value);
+            }
+
+            return SearchBinary(array, value);
+        }
+
+        private int SearchBinary(string[] array, string value)
+        {
+            int first = 0, last = array.Length - 1;
+
+            while(first <= last)
+            {
+                while (first <= last && array[last] == string.Empty)
+                {
+                    last--;
+                }
+
+                if (last < first)
+                {
+                    return -1;
+                }
+
+                int mid = (last + first) >> 1;
+
+                while (array[mid] == string.Empty)
+                {
+                    mid++;
+                }
+
+                int diff = array[mid].CompareTo(value);
+
+                if (diff == 0)
+                {
+                    return mid;
+                }
+                else if (diff < 0)
+                {
+                    first = mid + 1;
+                }
+                else if (diff > 0)
+                {
+                    last = mid - 1;
+                }
+            }
+
+            return -1;
+        }
+
+        private int SearchLinear(string[] array, string value)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (value == array[i])
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
