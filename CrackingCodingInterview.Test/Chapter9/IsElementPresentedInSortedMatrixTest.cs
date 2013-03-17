8dd373b6b8c96d341992593513553609f9698e6c
Oleksandr Lukiantsev <oleksandr.lukiantsev@gmail.com>
2013-03-16 17:05:19 -0700

    added #9.6

---
+++ CrackingCodingInterview.Test/Chapter9/IsElementPresentedInSortedMatrixTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using CrackingCodingInterview.Chapter9;
+using NUnit.Framework;
+
+namespace CrackingCodingInterview.Test.Chapter9
+{
+    [TestFixture]
+    public class IsElementPresentedInSortedMatrixTest : TestBase
+    {
+        private static IEnumerable TestCases
+        {
+            get
+            {
+                var matrix = new[,]
+                    {
+                        {1,2,3,4},
+                        {2,3,4,5},
+                        {3,4,5,6},
+                        {4,5,6,7}
+                    };
+
+                yield return
+                    new TestCaseData(matrix, 7).Returns(true);
+
+                yield return
+                    new TestCaseData(matrix, 1).Returns(true);
+
+
+                yield return
+                    new TestCaseData(matrix, 2).Returns(true);
+
+                yield return
+                    new TestCaseData(matrix, 0).Returns(false);
+
+
+                yield return
+                    new TestCaseData(matrix, 8).Returns(false);
+
+            }
+        }
+
+
+        [TestCaseSource("TestCases")]
+        [Test]
+        public bool Test(int[,] matrix, int value)
+        {
+            return RunTest(Tuple.Create(matrix, value), new IsElementPresentedInSortedMatrix());
+        }
+    }
+}
---
+++ CrackingCodingInterview/Chapter9/IsElementPresentedInSortedMatrix.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CrackingCodingInterview.Chapter9
+{
+
+    [Reference(
+        Page = 66
+        , Number = "9.6"
+        , Description = "Given a matrix in which each row and each column is sorted write a method to find an element in it"
+        )]
+    public class IsElementPresentedInSortedMatrix : Algorithm<Tuple<int[,], int>, bool>
+    {
+        protected override bool OnExecute(Tuple<int[,], int> arg)
+        {
+            int[,] matrix = arg.Item1;
+            int value = arg.Item2;
+
+            int row = 0;
+            int col = matrix.GetLength(1) - 1;
+
+            while (row < matrix.GetLength(0) && col >= 0)
+            {
+                int item = matrix[row, col];
+
+                if (item == value)
+                {
+                    return true;
+                }
+                
+                if (item > value)
+                {
+                    col--;
+                }
+                else
+                {
+                    row++;
+                }
+            }
+
+            return false;
+        }
+    }
+}
