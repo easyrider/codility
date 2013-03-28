89c83d9a8e22eef0e46edf8f422b3a3e0008c92c
Oleksandr Lukiantsev <oleksandr.lukiantsev@gmail.com>
2013-03-28 15:31:16 -0700

    added #10.7

---
+++ CrackingCodingInterview.Test/Chapter10/GetKthMagicNumberTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Linq;
+using CrackingCodingInterview.Chapter10;
+using NUnit.Framework;
+
+namespace CrackingCodingInterview.Test.Chapter10
+{
+    [TestFixture]
+    public class GetKthMagicNumberTest : TestBase
+    {
+        private static IEnumerable TestCases
+        {
+            get
+            {
+                int[] magics = {0, 1, 3, 5, 7, 9, 15, 21, 25, 27, 35, 45, 49, 63};
+
+                return magics.Select((x, i) => new TestCaseData(i).Returns(x));
+            }
+        }
+
+
+        [TestCaseSource("TestCases")]
+        [Test]
+        public int Test(int value)
+        {
+            int result = RunTest(value, new GetKthMagicNumber());
+            return result;
+        }
+    }
+}
---
+++ CrackingCodingInterview/Chapter10/GetKthMagicNumber.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CrackingCodingInterview.Chapter10
+{
+
+     [Reference(
+       Page = 68
+       , Number = "10.7"
+       , Description = "Design an algorithm to find the kth number such that the only prime factors are 3, 5, and 7."
+       )]
+    public class GetKthMagicNumber : Algorithm<int, int>
+    {
+        protected override int OnExecute(int arg)
+        {
+            if (arg == 0)
+            {
+                return 0;
+            }
+
+            int value = 1;
+
+            Queue<int> q3 = new Queue<int>(new[] { 3 });
+            Queue<int> q5 = new Queue<int>(new[] { 5 });
+            Queue<int> q7 = new Queue<int>(new[] { 7 });
+
+            for (--arg; arg > 0; --arg)
+            {
+                value = Math.Min(q3.Peek(), Math.Min(q5.Peek(), q7.Peek()));
+                
+                if (value == q7.Peek())
+                {
+                    q7.Dequeue();
+                }
+                else
+                {
+                    if (value == q5.Peek())
+                    {
+                        q5.Dequeue();
+                    }
+                    else
+                    {
+                        q3.Dequeue();
+                        q3.Enqueue(value * 3);
+                    }
+
+                    q5.Enqueue(value * 5);
+                }
+
+                q7.Enqueue(value * 7);
+            }
+
+            return value;
+        }
+    }
+}
